3dc5e39bd53a129b0bfde7b3e84b6b3337a7bc1d
Sajid Jaber <sajidjaber@yahoo.com>
2024-01-25 10:12:53 -0600

    words.py setup

---
+++ words.py
@@ -0,0 +1,9 @@
+
+def print_upper_words(words, must_start_with):
+
+    for word in words:
+        for letter in must_start_with:
+            if word.startswith(letter):
+                print(word.upper())
+                break
+
